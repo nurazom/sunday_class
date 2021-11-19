eb9215c7f57d83aae310b84e2ab9644dc5c29971
nurazom <88337901+nurazom@users.noreply.github.com>
2021-11-20 04:26:01 +0600

    Spalash Screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:sunday_class/Timeline.dart';
 
 class Splash_Screen extends StatefulWidget {
   const Splash_Screen({Key key}) : super(key: key);
@@ -12,20 +13,36 @@ class _Splash_ScreenState extends State<Splash_Screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage("images/cover_image.jpg")),
-            shape: BoxShape.rectangle,
-            color: Colors.limeAccent,
-            border: Border.all(
-              color: Colors.lightBlue,
+      body: Align(
+        //alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("images/cover_image.jpg")),
+                shape: BoxShape.rectangle,
+                color: Colors.limeAccent,
+                border: Border.all(
+                  color: Colors.lightBlue,
+                ),
+              ),
+              height: 100,
+              width: 120,
+              // color: Colors.indigo.shade100,
             ),
-          ),
-          height: 100,
-          width: 120,
-          // color: Colors.indigo.shade100,
-          child: Column(),
+            Container(
+                child: IconButton(
+                    icon: Icon(Icons.arrow_forward),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => TimelinePage()));
+                    })),
+          ],
         ),
       ),
     );
